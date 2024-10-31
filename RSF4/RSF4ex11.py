5c65d4ecc9eb57fc77d7e3f05e8e15c5f4d463dc
Miguyy <miguelangelo123machado@gmail.com>
2024-10-31 14:30:27 +0000

    alterações ex 11 ficha 4

--- RSF4/RSF4ex11.py
+++ RSF4/RSF4ex11.py
@@ -2,5 +2,5 @@ def printCharLine(texto, numeroCar):
     for i in range(0, len(texto), numeroCar):
         print(texto[i:i + numeroCar]) 
 texto = input("Escreve aqui: ")
-numeroCar = 10
+numeroCar = int(input("Quantos por linha?: "))
 printCharLine(texto, numeroCar)
